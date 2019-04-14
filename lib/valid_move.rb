3fb090107c41b8fa719ce5f27efcd01f737f9189
Tomas Gunz <tagunz@hotmail.com>
2019-04-14 18:27:09 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,25 @@
+# code your #position_taken? method here!
+
+board = ["X"," "," "," "," "," "," "," "," "]
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    puts valid_move?
+  end
+end
+
+puts position_taken?(board, 11)
+puts position_taken?(board, 1)
+puts position_taken?(board, 3)
+
 # code your #valid_move? method here
-
-
+def valid_move?(board, index)
+  if position_between?(0-8)
+   print "this block is already filled"
+   true
+  else
+    print "invalid selection"
+    return false
+  end
+end  
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
